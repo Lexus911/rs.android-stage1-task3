e8e986bc9de5f78c5e20fecd58d2a133426bc66d
Alexey Matskevich <61157700+Lexus911@users.noreply.github.com>
2020-05-06 23:20:49 +0300

    feat: add solution for subtask5

--- src/main/kotlin/subtask5/TelephoneFinder.kt
+++ src/main/kotlin/subtask5/TelephoneFinder.kt
@@ -4,8 +4,61 @@ import kotlin.reflect.KClass
 
 class TelephoneFinder {
 
-    // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+
+        var result = emptyArray<String>()
+        for(i in 0 until number.length-1) {
+            if (number[i] == '1') {
+                result += number.replaceRange(i, i + 1, "2")
+                result += number.replaceRange(i, i + 1, "4")
+            }
+            if (number[i] == '2') {
+                result += number.replaceRange(i, i + 1, "1")
+                result += number.replaceRange(i, i + 1, "3")
+                result += number.replaceRange(i, i + 1, "5")
+            }
+            if (number[i] == '3') {
+                result += number.replaceRange(i, i + 1, "2")
+                result += number.replaceRange(i, i + 1, "6")
+            }
+            if (number[i] == '4') {
+                result += number.replaceRange(i, i + 1, "1")
+                result += number.replaceRange(i, i + 1, "5")
+                result += number.replaceRange(i, i + 1, "7")
+            }
+            if (number[i] == '5') {
+                result += number.replaceRange(i, i + 1, "2")
+                result += number.replaceRange(i, i + 1, "4")
+                result += number.replaceRange(i, i + 1, "6")
+                result += number.replaceRange(i, i + 1, "8")
+            }
+            if (number[i] == '6') {
+                result += number.replaceRange(i, i + 1, "3")
+                result += number.replaceRange(i, i + 1, "5")
+                result += number.replaceRange(i, i + 1, "9")
+            }
+            if (number[i] == '7') {
+                result += number.replaceRange(i, i + 1, "4")
+                result += number.replaceRange(i, i + 1, "8")
+            }
+            if (number[i] == '8') {
+                result += number.replaceRange(i, i + 1, "5")
+                result += number.replaceRange(i, i + 1, "7")
+                result += number.replaceRange(i, i + 1, "0")
+                result += number.replaceRange(i, i + 1, "9")
+            }
+            if (number[i] == '9') {
+                result += number.replaceRange(i, i + 1, "6")
+                result += number.replaceRange(i, i + 1, "8")
+            }
+            if (number[i] == '0') {
+                result += number.replaceRange(i, i + 1, "8")
+            }
+        }
+
+        if(number[0] == '-')
+        return null
+        else
+        return result
     }
 }
