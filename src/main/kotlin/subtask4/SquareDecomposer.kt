e40edcd31147d0c30aecee1bde2c4c0322df874f
Alexey Matskevich <61157700+Lexus911@users.noreply.github.com>
2020-05-06 23:19:56 +0300

    feat: add solution for subtask4

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -2,8 +2,35 @@ package subtask4
 
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return decomposeRec(number * number, number - 1);
+
+    }
+
+    fun decomposeRec(rest: Int, number: Int): Array<Int>? {
+
+        for (i in number downTo 1) {
+            var sq = i * i;
+            var rest = rest - sq;
+            if (rest == 0) {
+                return arrayOf(i);
+            }
+            if (rest < 0) {
+                return null;
+            }
+
+            var square = kotlin.math.floor(kotlin.math.sqrt(rest.toDouble())).toInt();
+
+            if (square >= i) {
+                square = i - 1
+            }
+
+            var result = decomposeRec(rest, square);
+
+            if (result != null) return result + arrayOf(i)
+
+        }
+        return null;
+
     }
 }
