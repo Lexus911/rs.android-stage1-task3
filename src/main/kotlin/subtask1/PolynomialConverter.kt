8857fbac683eb091b0930804b632b423b3f99417
Alexey Matskevich <61157700+Lexus911@users.noreply.github.com>
2020-05-06 23:16:26 +0300

    feat: add solution for subtask1

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -1,9 +1,32 @@
 package subtask1
-
 class PolynomialConverter {
 
-    // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        val a = numbers.size - 1
+        var s = ""
+        for (i in a downTo 0 step 1) {
+            s += if (numbers[a - i] == 0) ""
+            else if ((a - i) == 0) "${numbers[a - i]}" + "x^${i}"
+            else if(numbers[a-i] == 1 ) " + "  +
+                    if(i > 1) "x^${i}"
+                    else if(i == 1) "x"
+                    else ""
+            else if(numbers[a-i] == -1 ) " - "  +
+                    if(i > 1) "x^${i}"
+                    else if(i == 1) "x"
+                    else ""
+            else if (numbers[a - i] > 0) " + " + "${numbers[a - i]}" +
+                    if (i > 1) "x^${i}"
+                    else if (i == 1) "x"
+                    else ""
+            else " - " + "${numbers[a - i] * (-1)}" +
+                    if (i > 1) "x^${i}"
+                    else if (i == 1) "x"
+                    else ""
+        }
+        return if(a > 0)
+            s
+        else
+            null
     }
 }
